9c9272cfc1a6227f2630775df6aa53cf7ac871a7
Anish <anishkumardev4@gmail.com>
2026-06-07 03:58:42 +0530

    fix: add netlify.toml

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
   base: "/",
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
-    react(),
+    viteReact(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: "./src/start.ts",
     },
   },
 });
